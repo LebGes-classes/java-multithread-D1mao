d3a901662320fd8d9f1dc9f175e1b76ec9dd29c8
D1mao <grinlight53721@mail.ru>
2024-05-20 10:35:19 +0300

    Homework

---
+++ Multithread/src/main/java/Multithread/Task.java
@@ -0,0 +1,30 @@
+package Multithread;
+
+public class Task {
+    public String name; // название задачи
+
+    public int idWorker; // айди исполнителя
+    public int hours; //время в часах, выделенное на исполнение задачи (>=1 &&<= 16)
+    public boolean status; //статус задачи (false, если задача выполнена)
+
+    public static final String TITLE_OF_TASK_TABLE = "tasks.xlsx";
+
+    public Task(String name, int time) {
+        this.hours = time;
+        this.status = true;
+        this.name = name;
+    }
+    public int getHours() {
+        return hours;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void complete() {
+        status = false;
+    }
+
+
+}
